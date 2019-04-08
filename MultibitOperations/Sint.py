f36cb6850e30d5760f41a3cc14451aadbf68e0a9
Marcin Ziajkwski <ziajkowski.marcin@gmail.com>
2019-04-08 17:32:42 +0200

    fixed dec counter

--- MultibitOperations/Sint.py
+++ MultibitOperations/Sint.py
@@ -7,13 +7,42 @@ class Sint:
         Octal = 2;
         Decimal = 3; # only decimal is supported right now
         Hexadecimal = 4;
-    
+    def hex_to_dec(self,hexi:str):
+        hexi = ord(hexi)
+        if (hexi >= ord('a') and hexi <= ord('f')):
+            tmp = hexi - ord('a') + 10
+        elif (hexi >= ord('A') and hexi <= ord('F')):
+            tmp = hexi - ord('A') + 10
+        else:
+            tmp = int(chr(hexi))
+        return tmp
+    def dec_to_hex(self,dec):
+        # dec have to be in range -7 to 15
+        if (dec > 0):
+            if (dec <= 9):
+                return chr(dec+48)
+            else:
+                return chr(ord('a')+(dec-10))
+        else:
+            return "0";
     __value = "0";
     __size = 1;
     __representation = data_representation.Decimal;
     __UnSaveFastMode = False;
     
-    def __init__(self,number = "0"):
+    def __init__(self, number = "0", repres = None):
+        if repres is None:
+            self.__representation = self.data_representation.Decimal;
+            print(type(self.__representation))
+            print(self.__representation)
+        else:
+            
+            print(type(repres))
+            print(repres)
+            self.__representation = repres;
+            print(type(self.__representation))
+            print(self.__representation)
+            
         self.value = str(number);
         self.size = len(self.value);
     def __str__(self):
@@ -35,46 +64,57 @@ class Sint:
             if (self.__representation.name == 'Decimal'):
                 for el in input_value:
                     if not (el in ['0','1','2','3','4','5','6','7','8','9']):
-                        raise
+                        raise Exception("It is not Dec type");
             elif (self.__representation.name == 'Binary'):
                 for el in input_value:
                     if not (el in ['0','1']):
-                        raise
+                        raise Exception("It is not Bin type");
             elif (self.__representation.name == 'Octal'):
                 for el in input_value:
                     if not (el in ['0','1','2','3','4','5','6','7']):
-                        raise
+                        raise Exception("It is not Octal type");
             elif (self.__representation.name == 'Hexadecimal'):
                 for el in input_value:
                     if not (el in ['0','1','2','3','4','5','6','7','8','9','a','A','b','B','c','C','d','D','e','f','F']):
-                        raise
-            else: raise
+                        raise Exception("It is not Hexa type");
+            else:
+                print(self.__representation.name)
+                raise Exception("Wrong representation type #1");
         # END if
         if (self.__representation.name == 'Decimal'):
-            gora = self.value;
-            dol  = str(input_value);
+            print("deci add")
+            gora = self.value[::-1];
+            dol  = str(input_value)[::-1];
             wyn = ""
-            dluzszy =  max(len(gora),len(dol))
-            lg= len(gora)
-            ld=len(dol)
+            lg = len(gora)
+            ld = len(dol)
             if lg > ld:
-                dol = (lg - ld) * '0' + dol
+                dol = dol + (lg - ld) * '0'
+                ld = len(dol)
             else:
-                gora = (ld-lg) * '0' + gora
+                gora = gora + (ld - lg) * '0'
+                lg = len(gora)
             mem = 0
-            for it in range(dluzszy,0,-1):
-              dig = int(gora[it-1])+int(dol[it-1])
-              if dig>9 : 
-                dig = dig -10
-                wyn += chr(dig+mem+48)
+            
+            for it in range(0,lg):
+              dig = int(gora[it]) + int(dol[it]) + mem;
+              if dig >= 10 and dig <= 19:
                 mem = 1
+                dig = dig - 10
+                wyn += chr(dig + 48)
               else:
-                wyn += chr(dig+mem+48)
-                mem =0
+                mem = 0;
+                wyn += chr(dig + 48)
             if mem > 0:
                 wyn += chr(mem+48)
             self.value = wyn[::-1];
-        else: raise
+        elif (self.__representation.name == 'Octal'):
+            pass
+        elif (self.__representation.name == 'Binary'):
+            pass
+        elif (self.__representation.name == 'Hexadecimal'):
+           pass
+        else: raise Exception("Wrong representation type #2");
         # END if
     def SUB(self,input_value):
         
@@ -115,20 +155,16 @@ class Sint:
         pass;
     def NOT(self,input_value): 
         pass;
-    
-    
+
 # end class --------------------
 
 if __name__ == "__main__":
-    moja = Sint();
-    print("go: ",moja)
-    moja = "3" + moja;
-    print("go: ",moja)
-    elo = Sint("400") + moja;
-    print("go: ",elo)
-    elo = Sint("400") + "2340";
-    print("go: ",elo)
-    elo = Sint("400") + "2340";
-    print("go: ",elo)
+    
+    twoja1 = Sint("65535",Sint.data_representation.Decimal);
+    twoja2 = Sint("986881",Sint.data_representation.Decimal);
+    twoja2 = twoja2 + twoja1;
+    print("go: ",twoja2," ? 1052416")
 
+
+    
 
